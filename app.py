b906dc934143a69de18197947320cc8115450204
Pirez <gustavo.pires018@gmail.com>
2023-09-21 18:15:46 -0300

    Adicione a caixa de texto

--- app.py
+++ app.py
@@ -2,11 +2,21 @@
 from tkinter import *
 
 janela = Tk()
-janela.title("Cotação atual das moedas")
-texto_orientacao = Label(janela,text="Clique no butão para ver as contações das moedas")
-texto_orientacao.grid(column=0,row=0)
-checkbtn = Checkbutton(janela,text="butão")
-checkbtn.grid(column=4,row=4)
-
+# Criação dp tamanho da janela
+janela.geometry("400x400") 
+# Deixando ela limitada ao tamanho Altura e Largura
+janela.minsize(400, 400) 
+janela.maxsize(400, 400)
+# Titulo da tela
+janela.title("Login")
+# Criação da Label
+textoUsuario = Label(janela,text="Digite seu usuário: ")
+textoUsuario.grid(column=0,row=0)
+# Criação no texboxte
+textExample= Text(janela, height=1)
+textExample.grid(column=3,row=1)
+# Criação do botão
+botao = Button(janela,text="Ok")
+botao.grid(column=0,row=1)
 janela.mainloop()
 
