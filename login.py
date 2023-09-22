51efeba1520875853292865f2befe9cfed361409
Pirez <gustavo.pires018@gmail.com>
2023-09-22 13:00:21 -0300

    Adicionando as primeiras funcoes

--- login.py
+++ login.py
@@ -1,26 +1,43 @@
 from tkinter import *
 
-class Application():
-    def __init__(self,janela=None):
-        self.janela = janela
-        self.janela.title("Login")
-        self.janela.geometry("400x400")
+app = Tk()
+
+class Funcs():
+    def resultado_soma(self):
+        self.a=int(self.e1.get())
+        self.b=int(self.e2.get())
+        self.c = self.a + self.b
+        self.label["text"] = self.c
 
 
-        self.frame = Frame(self.janela)
-        self.frame.pack(side=TOP)
+class Application(Funcs):
+    def __init__(self):
+        self.app = app
 
-        self.e1 = Entry(self.frame, font="arial",width=10)
+        self.tela()
+        app.mainloop()
+
+    def tela(self):
+        self.app.title("Login")
+        self.app.geometry("400x300")
+
+        self.e1 = Entry(self.app, font="arial",width=10)
         self.e1.grid(row=0,column=0)
-        self.e2 = Entry(self.frame, font="arial",width=10)
+        self.e2 = Entry(self.app, font="arial",width=10)
         self.e2.grid(row=1,column=0)
 
-        self.btn = Button(self.frame,text="Entrar",width=5)
+        self.btn = Button(self.app,text="Entrar",width=5,command=self.resultado_soma)
         self.btn.grid(row=2,column=0)
 
-app = Tk()
-Application(janela=app)
-app.mainloop()
+
+        self.label = Label(self.app,text="Resultado")
+        self.label.grid(row=3,column=0)
+
+        
+
+Application()
+
+
 
 # ctrl + ; - comenta as linhas
 
