6d1e9962debac2d047d4f25728ffd8ac554625f1
Pirez <gustavo.pires018@gmail.com>
2023-09-24 02:26:01 -0300

    Adicionando conexção com o banco de dados

--- login.py
+++ login.py
@@ -1,4 +1,20 @@
 from tkinter import *
+import mysql.connector
+
+con = mysql.connector.connect(host='localhost', database='devoir',user='root',password='elco478780') 
+
+if con.is_connected():
+    db_info = con.get_server_info()
+    print("conectado ao servidor MySQL versão",db_info)
+    cursor = con.cursor()
+    cursor.execute("select database();")
+    linha = cursor.fetchone()
+    print("conectado ao banco de dados",linha)
+
+if con.is_connected():
+    cursor.close()
+    con.close()
+    print("conexão ao MySQL foi encerrada")
 
 
         # cores -----------------------------
