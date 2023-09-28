72fd7abf6ea0943826101660730e091047f66b17
Pirez <gustavo.pires018@gmail.com>
2023-09-28 13:17:32 -0300

    Remoção da tela de login

--- NewLogin.py
+++ NewLogin.py
@@ -41,65 +41,16 @@ checkbox = customtkinter.CTkCheckBox(janela, text="Salvar Login", font=("Arial",
 checkbox.pack(padx=10,pady=0)
 login = customtkinter.CTkButton(janela,text="Entrar",width=150)
 login.pack(padx=10,pady=10)
-cadastro = customtkinter.CTkButton(janela,text="Não tenho login",command=lambda:[telacadastro()][fechalogin()])
+cadastro = customtkinter.CTkButton(janela,text="Não tenho login",command=lambda:[fechalogin()][telacadastro()])
 cadastro.pack(padx=1,pady=10)
 naosei = customtkinter.CTkLabel(janela,text="Esqueci minha senha",width=20,height=20, text_color="blue",cursor="hand2")
 naosei.pack(padx=1,pady=0)
 
 janela.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-# app = Tk()
-
-# class Aplicativo():
-#     def __init__(self):
-#         self.app = app
-#         self.Tela()
-#         self.CaixaTexto()
-#         self.funcaoInsert()
-#         app.mainloop()
-
-#     def Tela (self):
-#         self.app.title('Login')
-#         self.app.geometry("410x410")
-
-#     def CaixaTexto (self):
-
-#         self.e1 = Entry(self.app,font="arial",width=20,relief=SOLID)
-#         self.e1.grid(row=0,column=1)
-#         self.e2 = Entry(self.app, font="arial",width=20,relief=SOLID)
-#         self.e2.grid(row=1,column=1)
-#         self.btn = Button(self.app,text="Cadastrar",width=10)   
-#         self.btn.grid(row=3,column=1)
-#     def funcaoInsert(self):
-#         self.con = mysql.connector.connect(host='localhost', database='devoir', user='root', password='elco478780')
+# class iBanco():
+#     def inBanco(self):
+#         self.con = mysql.connector.connect(host='localhost', database='devoir',user='root',password='elco478780') # adicionando as conexções com o banco de dados
 #         self.cursor = self.con.cursor()
-    
-#         # Obtenha os valores dos campos de entrada
-#         usuario = self.e1.get()
-#         senha = self.e2.get()
-
-#         # Use os valores dos campos de entrada na consulta SQL
-#         self.comando = f'INSERT INTO tbllogin (usuario, senha) VALUES ("{usuario}", "{senha}")'
-
-#         try:
-#             self.cursor.execute(self.comando)
-#             self.con.commit()
-#             self.cursor.close()
-#             self.con.close()
-#             print("Registro inserido com sucesso!")
-#         except mysql.connector.Error as e:
-#             print(f"Erro ao inserir registro: {e}")          
-
-# Aplicativo()
+#         self.cursor.close()       
+#         self.con.close()
